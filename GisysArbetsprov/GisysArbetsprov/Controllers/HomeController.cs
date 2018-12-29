b51735f51581871edd541a7661307e001ee6a792
AbelM <AbelMAbraha@hotmail.com>
2018-12-29 01:41:07 +0100

    Add Project to Github!

---
+++ GisysArbetsprov/GisysArbetsprov/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using GisysArbetsprov.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GisysArbetsprov.Controllers
+{
+    public class HomeController : Controller
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        public ActionResult Index()
+        {
+            
+            return View();
+        }
+        public JsonResult GetConsultants()
+        {
+            var BonusPott = 5000;
+            ConsultInformation consult = new ConsultInformation();
+            List<ConsultInformation> list = new List<ConsultInformation>();
+            var consultants = db.ConsultInformations.ToList();
+
+            foreach (var item in consultants)
+            {
+                var editUser = db.ConsultInformations.Find(item.Id);
+                editUser.Bonus = Convert.ToInt32(BonusPott * (((((DateTime.Now.Year - item.YearsOfEmployment.Year) / 10m) + 1) * item.Hours) / CreditPoint()));
+                db.Entry(editUser).State = EntityState.Modified;
+                db.SaveChanges();
+            }
+            return Json(consultants, JsonRequestBehavior.AllowGet);
+        }
+        [HttpPost]
+        public ActionResult Index(ConsultInformation consInfo)
+        {
+            db.ConsultInformations.Add(new ConsultInformation { Name = consInfo.Name, Hours = consInfo.Hours, YearsOfEmployment = Convert.ToDateTime(consInfo.YearsOfEmployment.ToString("yyyy-MM-dd")) });
+            db.SaveChanges();
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            return View(db.ConsultInformations.Find(id));
+        }
+        [HttpPost]
+        public ActionResult Edit(ConsultInformation consInfo)
+        {
+            var editConsultant = db.ConsultInformations.Find(consInfo.Id);
+            editConsultant.Hours = consInfo.Hours;
+            editConsultant.Name = consInfo.Name;
+            editConsultant.YearsOfEmployment = Convert.ToDateTime(consInfo.YearsOfEmployment.ToString("yyyy-MM-dd"));
+            db.Entry(editConsultant).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Delete(int id)
+        {
+            var deleteConsultant = db.ConsultInformations.Find(id);
+            db.ConsultInformations.Remove(deleteConsultant);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public decimal CreditPoint()
+        {
+            decimal creditPoint = 0;
+            var consultants = db.ConsultInformations.ToList();
+            foreach (var item in consultants)
+            {
+                if ((DateTime.Now - item.YearsOfEmployment).TotalDays < 365)
+                {
+                    creditPoint += item.Hours;
+                }
+                else
+                {
+                    creditPoint += (((DateTime.Now.Year - item.YearsOfEmployment.Year) / 10m) + 1) * item.Hours;
+                }
+            }
+            return decimal.Truncate(creditPoint);
+        }
+    }
+}
---
+++ GisysArbetsprov/GisysArbetsprov/Models/ConsultInformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GisysArbetsprov.Models
+{
+    public class ConsultInformation
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime YearsOfEmployment { get; set; }
+        public int Hours { get; set; }
+        public int Bonus { get; set; }
+    }
+}
